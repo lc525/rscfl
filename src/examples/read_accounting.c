9c3365405c96dc87a2fecccb575537cedc615837
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2014-05-19 14:08:47 +0100

    Initial upload of some rough ideas as to how resourceful works, and its directory structure

---
+++ src/examples/read_accounting.c
@@ -0,0 +1,40 @@
+#define BIT(x) 1U << x
+#include "costs.h"
+#include <sys/mman.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#define RESOURCEFUL_FILE "/mnt/resourceful0"
+
+int main(int argc, char *argv[])
+{
+	struct accounting *acc;
+	const char *memblock;
+	struct stat sb;
+	int fd;
+	int size;
+
+	if ( (fd = open(RESOURCEFUL_FILE, O_RDONLY)) == -1) {
+		printf("Cannot open %s\n", RESOURCEFUL_FILE);
+		return -1;
+	}
+	fstat(fd, &sb);
+	size = sb.st_size;
+	printf("size=%d\n", size);
+	memblock = mmap((caddr_t) 0, size, PROT_READ, 0, fd, 0);
+	if (memblock == MAP_FAILED) {
+		printf("Mmap failed: %d\n", errno);
+		return errno;
+	}
+
+	while (1) {
+		acc = (struct accounting *)memblock;
+		printf("CPU cycles: %llu\n", acc->cpu.cycles);
+		sleep(100);
+	}
+	
+	return 0;
+}
---
+++ src/examples/small-big-write-sync.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+include "resourceful.h"
+
+#define small 1
+#define large 10
+
+
+int main(int argc, char *argv[])
+{
+	int fd = open("/home/oc243/file.bin", O_ASYNC | O_NONBLOCK);
+	char small_data[small]; // Unitialized
+	char large_data[large]; // Uninialized
+	write(fd, small_data, small);
+	write(fd, large_data, large);
+	fsync(fd);
+	close(fd);
+	
+	return 0;
+}

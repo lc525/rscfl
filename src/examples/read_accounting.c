a9a7a083e943311a27f12bf7990e5cd8b7484441
Oliver Chick <oc243@cam.ac.uk>
2014-05-22 17:40:47 +0100

    Indent socket_tester as per house style

--- src/examples/read_accounting.c
+++ src/examples/read_accounting.c
@@ -11,30 +11,30 @@
 
 int main(int argc, char *argv[])
 {
-	struct accounting *acc;
-	const char *memblock;
-	struct stat sb;
-	int fd;
-	int size;
+  struct accounting *acc;
+  const char *memblock;
+  struct stat sb;
+  int fd;
+  int size;
 
-	if ( (fd = open(RESOURCEFUL_FILE, O_RDONLY)) == -1) {
-		printf("Cannot open %s\n", RESOURCEFUL_FILE);
-		return -1;
-	}
-	fstat(fd, &sb);
-	size = sb.st_size;
-	printf("size=%d\n", size);
-	memblock = mmap((caddr_t) 0, size, PROT_READ, 0, fd, 0);
-	if (memblock == MAP_FAILED) {
-		printf("Mmap failed: %d\n", errno);
-		return errno;
-	}
+  if ( (fd = open(RESOURCEFUL_FILE, O_RDONLY)) == -1) {
+    printf("Cannot open %s\n", RESOURCEFUL_FILE);
+    return -1;
+  }
+  fstat(fd, &sb);
+  size = sb.st_size;
+  printf("size=%d\n", size);
+  memblock = mmap((caddr_t) 0, size, PROT_READ, 0, fd, 0);
+  if (memblock == MAP_FAILED) {
+    printf("Mmap failed: %d\n", errno);
+    return errno;
+  }
 
-	while (1) {
-		acc = (struct accounting *)memblock;
-		printf("CPU cycles: %llu\n", acc->cpu.cycles);
-		sleep(100);
-	}
+  while (1) {
+    acc = (struct accounting *)memblock;
+    printf("CPU cycles: %llu\n", acc->cpu.cycles);
+    sleep(100);
+  }
 	
-	return 0;
+  return 0;
 }
--- src/examples/socket_tester.c
+++ src/examples/socket_tester.c
@@ -4,32 +4,32 @@
 
 int main(int argc, char *argv[])
 {
-	int DEBUG = 0;
-	int sock_domain = AF_LOCAL;
-	int sock_type = SOCK_RAW;
-	int sock_proto = 0;
+  int DEBUG = 0;
+  int sock_domain = AF_LOCAL;
+  int sock_type = SOCK_RAW;
+  int sock_proto = 0;
 
-	int socfd_1;
-	int socfd_2;
-	int socfd_3;
+  int socfd_1;
+  int socfd_2;
+  int socfd_3;
 
-	if (DEBUG)
-		printf("Opening sockets\n");
+  if (DEBUG)
+    printf("Opening sockets\n");
 
-	// Open 3 sockets
-	socfd_1 = socket(sock_domain, sock_type, sock_proto);
+  // Open 3 sockets
+  socfd_1 = socket(sock_domain, sock_type, sock_proto);
 
-	acct_next();
-	socfd_2 = socket(sock_domain, sock_type, sock_proto);
-	read_acct();
+  acct_next();
+  socfd_2 = socket(sock_domain, sock_type, sock_proto);
+  read_acct();
 
-	socfd_3 = socket(sock_domain, sock_type, sock_proto);
+  socfd_3 = socket(sock_domain, sock_type, sock_proto);
 
-	if (DEBUG)
-		printf("Closing sockets\n");
+  if (DEBUG)
+    printf("Closing sockets\n");
 
-	// Be a good citizen
-	close(socfd_1);
-	close(socfd_2);
-	close(socfd_3);
+  // Be a good citizen
+  close(socfd_1);
+  close(socfd_2);
+  close(socfd_3);
 }

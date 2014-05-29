cd17bc7e837d94f4b843356b3a8b73d6d6405387
Lucian Carata <lc525@cam.ac.uk>
2014-05-29 12:27:00 +0100

    Update .rscfl_ko_update.sh
    When called without any arguments, it just removes currently loaded
    rscfl kernel modules and kills the programs using them.
    
    When called with an argument (a .ko), it removes loaded modules as above
    and then inserts the given .ko using staprun

--- src/build/.rscfl_ko_update.sh
+++ src/build/.rscfl_ko_update.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 function print_usage {
-  echo "$0 <rscfl_new_ko>"
+  echo "$0 [rscfl_new_ko]"
   echo ""
-  echo "removes any running rscfl_*.ko (killing any applications that"
-  echo "use it), and inserts the new module into the kernel"
+  echo "- rmmods any rscfl_*.ko kernel module (killing applications that use it)"
+  echo "- when given a kernel module as a parameter, it inserts it into the"
+  echo "  kernel using staprun"
 }
 
-if [ $# -lt 1 ]
-then
-  print_usage
-  exit 1
-fi
 MODNAME=`lsmod | grep rscfl | awk '{ print $1; }'`
+
+while getopts ":h" opt; do
+  case $opt in
+    h)
+      print_usage
+      exit 0
+      ;;
+  esac
+done
 
 if [ ! -z "$MODNAME" ]
 then
@@ -29,6 +34,10 @@ then
   sudo rmmod $MODNAME
 fi
 
-#insert new module
-echo "running new module ($1) with staprun..."
-sudo staprun $1 &
+if [ $# -eq 1 ]
+then
+  #insert new module
+  echo "running new module ($1) with staprun..."
+  sudo staprun $1 &
+fi
+

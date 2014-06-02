5e449620b302543e5e7faf90218381cb83f6cb2d
Oliver Chick <oc243@cam.ac.uk>
2014-06-02 18:03:05 +0100

    Check return values of socket

--- src/examples/socket_tester.c
+++ src/examples/socket_tester.c
@@ -21,21 +21,28 @@ int main(int argc, char *argv[])
   char *relay_f_data;
   struct accounting acct = {0};
 
-  // Open 3 sockets
-  socfd_1 = socket(sock_domain, sock_type, sock_proto);
-
+// Open 3 sockets
   rscfl_init(&relay_f_data);
+
+  if ( (socfd_1 = socket(sock_domain, sock_type, sock_proto)) < 0) {
+    goto soc_err;
+  }
+
   if (rscfl_acct_next()) {
     fprintf(stderr, "rscfl: acct_next errno=%d", errno);
     return -1;
   }
 
-  socfd_2 = socket(sock_domain, sock_type, sock_proto);
+  if ( (socfd_2 = socket(sock_domain, sock_type, sock_proto)) < -1 ) {
+    goto soc_err;
+  }
 
   rscfl_read_acct(relay_f_data, &acct);
   printf("Acct: %llu\n", acct.cpu.cycles);
 
-  socfd_3 = socket(sock_domain, sock_type, sock_proto);
+  if ( (socfd_3 = socket(sock_domain, sock_type, sock_proto)) < -1 ) {
+    goto soc_err;
+  }
 
   if (DEBUG)
     printf("Closing sockets\n");
@@ -45,4 +52,8 @@ int main(int argc, char *argv[])
   close(socfd_2);
   close(socfd_3);
   return 0;
+
+soc_err:
+  fprintf(stderr, "rscfl: error creating sockets\n");
+  return -1;
 }

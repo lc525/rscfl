6c7be2f390d8ae7f927d839d6fd3d7fc664757d2
Oliver Chick <oc243@cam.ac.uk>
2014-06-10 14:56:40 +0100

    res_api: don't check pid.
    We have a separate mapping per pid, so this check is now redundant

--- src/lib/res_api.c
+++ src/lib/res_api.c
@@ -113,8 +113,7 @@ int rscfl_read_acct(rscfl_handle relay_f_data, struct accounting *acct)
   struct accounting *relay_acct = (struct accounting *) relay_f_data->buf;
   while (relay_acct - (struct accounting *) relay_f_data->buf <
 	 NO_RELAY_ACCTS) {
-    if ((relay_acct->syscall_id.pid == getpid()) &&
-	(relay_acct->syscall_id.id == (relay_f_data->lst_syscall.id - 1))) {
+    if (relay_acct->syscall_id.id == (relay_f_data->lst_syscall.id - 1)) {
       memcpy(acct, relay_acct, sizeof(struct accounting));
       return 0;
     }

b6d6060035584817561da1e5df9569ea74562c8b
James Snee <jas250@cam.ac.uk>
2014-06-17 13:48:07 +0100

    Example program to help validate the output of resourceful. Connects a socket to a server in Australia and uses Resourceful to output the consumption. Next addition is to connect to a closer / local server

---
+++ src/examples/value_validation.c
@@ -0,0 +1,74 @@
+#include <errno.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <res_user/res_api.h>
+#include <costs.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+	rscfl_handle r_handle;
+	struct accounting acct_1 = {0};
+	int sockfd_1;
+	int sockfd_2;
+
+	printf("Init rscfl\n");
+	if (!(r_handle = rscfl_init())) {
+		fprintf(stderr, "rscfl: Init error. Errno=%d\n", errno);
+		return -1;
+	}
+
+  /*
+   * SOCKET 1
+   */
+	// Open the first socket and bind it to Australia
+  if ((sockfd_1 = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    goto soc_err;
+
+  struct sockaddr_in serv_addr_1;
+  memset(&serv_addr_1, 0, sizeof(serv_addr_1));
+  serv_addr_1.sin_family = AF_INET;
+  serv_addr_1.sin_port = htons(80);
+  if (inet_pton(AF_INET, "129.94.242.51", &serv_addr_1.sin_addr) <= 0) {
+    fprintf(stderr, "inet_pton error. Errno=%d\n", errno);
+    return -1;
+  }
+
+  //RSCFL
+  if (rscfl_acct_next(r_handle)) {
+    fprintf(stderr, "rscfl: acct_next errno=%d\n", errno);
+    return -1;
+  }
+
+  if (connect(sockfd_1, (struct sockaddr *)&serv_addr_1, sizeof(serv_addr_1)) < 0) {
+    fprintf(stderr, "Error connecting socket. Errno=%d\n", errno);
+    return -1;
+  }
+
+	// RSCFL	
+	if (!rscfl_read_acct(r_handle, &acct_1)) {
+		printf("rscfl: cpu_cycles=%llu wall_clock_time=%llu\n", acct_1.cpu.cycles, acct_1.cpu.wall_clock_time);
+	} else {
+		fprintf(stderr, "rscfl: read_acct failed\n");
+	}
+
+
+  /*
+   * SOCKET 2
+   */
+
+
+	// Clean up
+	close(sockfd_1);
+	return 0;
+
+soc_err:
+  fprintf(stderr, "Socket error. errno=%d\n", errno);
+  return -1;
+}

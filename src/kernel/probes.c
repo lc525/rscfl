e372dd1bec5068c2bccacca18db30ae1e1b84c32
Lucian Carata <lc525@cam.ac.uk>
2014-07-16 16:04:05 +0100

    run begin/end probes with preemption enabled, to avoid kernel crashes
    avoids "BUG: scheduling while atomic" kernel crashes during module initialization
    cherry-picked from lc525-experimental

--- src/kernel/probes.c
+++ src/kernel/probes.c
@@ -3,28 +3,45 @@
 
 int probes_init(void)
 {
-  int rc = (_rscfl_shim_init());
-  if (rc) {
-    return rc;
+  int rc1 = 0, rc2 = 0;
+
+  // stap disables preemption even when running begin/end probes.
+  // however, _rscfl_shim_init and/or _netlink_setup might sleep,
+  // causing an BUG: scheduled in atomic section kernel error (for
+  // sleeping with preemption disabled).
+  //
+  // because we know what we're doing, temporarily enable
+  // scheduling here.
+  // TODO(lc525): see if we need to restore IRQs as well
+  //
+  preempt_enable();
+  rc1 = _rscfl_shim_init();
+  rc2 = _netlink_setup();
+  preempt_disable();
+  if (rc1) {
+    printk(KERN_ERR "rscfl: cannot initialize rscfl driver\n");
   }
-  rc = _netlink_setup();
-  if (rc) {
-    return rc;
+  if (rc2) {
+    printk(KERN_ERR "rscfl: cannot initialize netlink\n");
   }
-  return 0;
+  return (rc1 | rc2) ;
 }
 
 int probes_cleanup(void)
 {
-  int rc =_rscfl_shim_cleanup();
-  if (rc) {
+  int rc1 = 0, rc2 = 0;
+
+  // see comment in probes_init for why we need to explicitly enable
+  // preemption here
+  preempt_enable();
+  rc1 =_rscfl_shim_cleanup();
+  rc2 = _netlink_teardown();
+  preempt_disable();
+  if (rc1) {
     printk(KERN_ERR "rscfl: cannot cleanup rscfl driver\n");
-    return rc;
   }
-  rc = _netlink_teardown();
-  if (rc) {
+  if (rc2) {
     printk(KERN_ERR "rscfl: cannot teardown netlink\n");
-    return rc;
   }
-  return 0;
+  return (rc1 | rc2);
 }

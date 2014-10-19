9dade9b47862d0be225f8b53a8e3ea2d095c169a
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2014-10-19 12:50:06 +0100

    Major refactor of subsys_static_discovery to use MAINTAINERS file.
    Previously we identified a subsystem as a top-level directory, but this
    is hugely course, and generally a bad idea.  This patch defines a
    subsystem by using the subsystems defined in the MAINTAINERS file. This
    gives us a much more fine-grained notion of a subsystem. Moreover, any
    large repo should have a similar file, so our approach can be adapted to
    another operating system.
    
    The script now finds all callq functions by scanning objdump. For each
    function, it takes the addresses, and pumps them through addr2line to
    find which file they are in. I then call get_maintainers.pl, which
    identifies the subsystem they're in from the MAINTAINERS file.

--- src/scripts/subsys_discovery/subsys_static_discovery.py
+++ src/scripts/subsys_discovery/subsys_static_discovery.py
@@ -1,66 +1,104 @@
 #!/usr/bin/env python2.7
 
-import os
-import sys
 import argparse
 import re
 import subprocess
-from subprocess import PIPE
 
-def read_cscope(p):
-    lines = []
-    line = p.stdout.readline()
-    pat = re.compile(">> cscope: ([0-9]*) lines")
-    m = pat.match(line)
-    for i in range(int(m.group(1))):
-        lines.append(p.stdout.readline())
-    return lines
+file_subsys_cache = {}
+addr_line_cache = {}
+
+def get_subsys(addr, addr2line, linux):
+    # Use addr2line to convert addr to the filename that it is in.
+    #
+    # Args:
+    #     addr: an address into the linux kernel.
+    #     addr2line: an already-opened subprocess to addr2line, which we can
+    #         use to map addr to a source code file.
+    #     file_subsys_cache: a cache that maps files to subsystems that they're
+    #         contained in. Used to prevent repeated calls to get_maintainer.pl.
+    #     linux: string location of the Linux kernel.
+    # Returns:
+    #     A string representing the name of the subsystem that addr is located
+    #     in.
 
 
-def get_subsys(fn, p):
-    subsys = []
-    p.stdin.write("1%s\n" % fn)
-    for line in read_cscope(p):
-        subsys.append(line.split('/')[0])
-    return subsys
+    # Have we already mapped addr to a file name? If so use the cached value,
+    # to save expensive calls out to addr2line.
+    if addr in addr_line_cache:
+        file_name = addr_line_cache[addr]
+    else:
+        addr2line.stdin.write("%s\n" % addr)
+        file_line = addr2line.stdout.readline()
+        if file_line.startswith("??:"):
+            # addr2line can't map this address.
+            return None
+        # Convert file:line to file
+        file_name = file_line.split(":")[0]
+
+        # Make filename relative
+        file_name = file_name.split("linux-stable/")[1]
+        addr_line_cache[addr] = file_name
+
+    # Check the cache of files we've already found a subsystem for.
+    if file_name in file_subsys_cache:
+        return file_subsys_cache[file_name]
+    else:
+        # Use the get_maintainer.pl script to check the MAINTAINERS file,
+        # to get the subsystem.
+        proc = subprocess.Popen(["%s/scripts/get_maintainer.pl" % linux,
+                                 "--subsystem", "--noemail",
+                                 "--no-remove-duplicates", "--no-rolestats",
+                                 "-f", "%s" %  file_name], cwd=linux,
+                                 stdout=subprocess.PIPE)
+        (stdout, stderr) = proc.communicate()
+        maintainers = stdout.strip().split("\n")
+        subsys = ""
+        # The most specific subsystem is listed straight after linux-kernel
+        # mailing list.
+        for i, line in enumerate(maintainers):
+            if line.startswith("linux-kernel@vger.kernel.org"):
+                subsys = maintainers[i+1]
+                break
+        file_subsys_cache[file_name] = subsys
+        return subsys
 
 
-def get_fn_calls(linux):
-    subsys_cache = {}
+def get_addresses_of_boundary_calls(linux):
+    # Find the addresses of all call instructions that operate across a kernel
+    # subsystem boundary.
+    #
+    # Args:
+    #     linux: string representing the location of the linux kernel.
+    #
+    # Returns:
+    #     a dictionary (indexed by subsystem name) where each element is a list
+    #     of addresses that are callq instructions whose target is in the
+    #     appropriate subsystem.
     boundary_fns = {}
-    cscope = subprocess.Popen(["cscope -dl cscope.out"],
-                         shell=True, stdout=PIPE, stdin=PIPE, stderr=PIPE,
-                         cwd=linux)
-    proc1 = subprocess.Popen('objdump -d vmlinux', shell=True,
-                             stdout=subprocess.PIPE, cwd=linux)
-    proc2 = subprocess.Popen(
-        ['grep', '-E', 'callq|>:'], stdin=proc1.stdout, stdout=PIPE)
-    out, err = proc2.communicate()
-    p = re.compile("([0-9a-f]{16,16}) <(.*)>:")
-    p_callq = re.compile("([0-9a-f]{16,16}).*callq.*<(.*)>")
-    for line in out.split('\n'):
-        m = p.match(line)
+    addr2line = subprocess.Popen(['addr2line', '-e' '%s/vmlinux' % linux],
+                                 stdout=subprocess.PIPE,
+                                 stdin=subprocess.PIPE)
+    # Use objdump+grep to find all callq instructions.
+    proc = subprocess.Popen('objdump -d vmlinux', shell=True,
+                            stdout=subprocess.PIPE, cwd=linux)
+    # regex to match callq instructions, creating groups from the caller, and
+    # callee addresses.
+    p_callq = re.compile("([0-9a-f]{16,16}).*callq.*([0-9a-f]{16,16}).*$")
+    for line in proc.stdout:
+        m = p_callq.match(line)
         if m:
-            fn = m.group(2)
-            addr = m.group(1)
-            subsys = get_subsys(fn, cscope)
-        else:
-            m = p_callq.match(line)
-            if m:
-                callee_fn = m.group(2)
-                callee_addr = m.group(1)
-                try:
-                    callee_subsys = subsys_cache[callee_fn]
-                except KeyError:
-                    callee_subsys = get_subsys(callee_fn, cscope)
-                    subsys_cache[callee_fn] = callee_subsys
-                if get_subsys(callee_fn, cscope) != subsys:
-                    for x in callee_subsys:
-                        try:
-                            boundary_fns[x]
-                        except KeyError:
-                            boundary_fns[x] = []
-                        boundary_fns[x].append(callee_addr)
+            caller_addr = m.group(1)
+            callee_addr = m.group(2)
+
+            caller_subsys = get_subsys(caller_addr, addr2line, linux)
+            callee_subsys = get_subsys(callee_addr, addr2line, linux)
+            if not caller_subsys:
+                # Address that we can't map to source file.
+                continue
+            if callee_subsys != caller_subsys:
+                if callee_subsys not in boundary_fns:
+                    boundary_fns[callee_subsys] = []
+                boundary_fns[callee_subsys].append(caller_addr)
     return boundary_fns
 
 
@@ -71,18 +109,11 @@ def main():
                         Linux source directory""")
     args = parser.parse_args()
 
-    if not os.path.exists("%s/cscope.out" % args.linux_root):
-        sys.stderr.write("Cannot find cscope.out\n")
-        exit(-1)
-    subsys_entries = get_fn_calls(args.linux_root)
+    subsys_entries = get_addresses_of_boundary_calls(args.linux_root)
 
     for subsys in subsys_entries:
         entry_points = ['kernel.statement("%s"),' % x for x in
                         subsys_entries[subsys]]
-        # remove duplicates
-        entry_points = list(set(entry_points))
-        entry_points[-1] = entry_points[-1][0:-1]
-
         print("probe ")
         print("\n".join(entry_points))
         print("""

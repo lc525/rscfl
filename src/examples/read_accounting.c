61b57902838c2e0cc0fe13fd6e5fe68bc413a60b
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2014-10-27 11:18:24 +0000

    read_accounting: correct #include order.

--- src/examples/read_accounting.c
+++ src/examples/read_accounting.c
@@ -1,5 +1,4 @@
 #define BIT(x) 1U << x
-#include "rscfl/costs.h"
 
 #include <errno.h>
 #include <fcntl.h>
@@ -7,6 +6,8 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
+#include "rscfl/costs.h"
 
 #define RESOURCEFUL_FILE "/mnt/resourceful0"
 

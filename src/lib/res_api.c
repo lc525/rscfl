c8863e5f6d575a8faa13a4374b32f3d37c9494b6
Oliver Chick <oliver.chick--dtg-git@cl.cam.ac.uk>
2014-10-28 12:56:18 +0000

    Remove references to relayfs in our variable names

--- src/lib/res_api.c
+++ src/lib/res_api.c
@@ -125,29 +125,25 @@ int rscfl_read_acct(rscfl_handle rhdl, struct accounting *acct)
   int i = 0;
   if (rhdl == NULL) return -1;
 
-  struct accounting *relay_acct = (struct accounting *)rhdl->buf;
-  if (relay_acct != NULL) {
-    while (i < NO_RELAY_ACCTS) {
-      //printf("relay_acct: %p - in use: %lu\n",
-      //    (void *)relay_acct, relay_acct->in_use);
-      if (relay_acct->in_use == 1) {
-        if (relay_acct->syscall_id.id == (rhdl->lst_syscall.id - 1)) {
-          //printf("API read_acct from %p (syscall_id:%ld) pos:%d\n",
-          //    (void*)relay_acct, rhdl->lst_syscall.id-1, i);
-          memcpy(acct, relay_acct, sizeof(struct accounting));
-          relay_acct->in_use = 0;
+  struct accounting *shared_acct = (struct accounting *)rhdl->buf;
+  if (shared_acct != NULL) {
+    while (i < NO_RHDL_ACCTS) {
+      if (shared_acct->in_use == 1) {
+        if (shared_acct->syscall_id.id == (rhdl->lst_syscall.id - 1)) {
+          memcpy(acct, shared_acct, sizeof(struct accounting));
+          shared_acct->in_use = 0;
           return 0;
         } else {
-          relay_acct++;
+          shared_acct++;
           i++;
         }
       } else {
-        relay_acct++;
+        shared_acct++;
         i++;
       }
     }
   } else {
-    printf("relay_acct is null!\n");
+    printf("shared_acct is null!\n");
   }
   return -1;
 }
@@ -168,19 +164,19 @@ int rscfl_merge_acct(rscfl_handle rhdl, struct accounting *acct)
 
   // aliases into rhdl's and accts subsys_accountings.
   struct subsys_accounting* acct_subsys;
-  struct subsys_accounting* relay_subsys;
+  struct subsys_accounting* rhdl_subsys;
 
   if (rhdl == NULL) return -1;
 
-  struct accounting *relay_acct = (struct accounting *)rhdl->buf;
-  if (relay_acct != NULL) {
-    while (i < NO_RELAY_ACCTS) {
-      if (relay_acct->in_use) {
-        if (relay_acct->syscall_id.id == (rhdl->lst_syscall.id - 1)) {
+  struct accounting *rhdl_acct = (struct accounting *)rhdl->buf;
+  if (rhdl_acct != NULL) {
+    while (i < NO_RHDL_ACCTS) {
+      if (rhdl_acct->in_use) {
+        if (rhdl_acct->syscall_id.id == (rhdl->lst_syscall.id - 1)) {
 	  // Sum together accounting values in each subsystem that they're set.
 	  for (subsys_no = 0; subsys_no < NUM_SUBSYSTEMS; subsys_no++) {
 	    acct_subsys = acct->acct_subsys[subsys_no];
-	    relay_subsys = relay_acct->acct_subsys[subsys_no];
+	    rhdl_subsys = rhdl_acct->acct_subsys[subsys_no];
 	    // We are iterating over an array of pointers. These pointers will
 	    // be NULL if the subsystem has not been touched. We therefore just
 	    // continue.
@@ -189,33 +185,33 @@ int rscfl_merge_acct(rscfl_handle rhdl, struct accounting *acct)
 	      continue;
 	    }
 
-	    acct_subsys->cpu.cycles += relay_subsys->cpu.cycles;
+	    acct_subsys->cpu.cycles += rhdl_subsys->cpu.cycles;
 	    acct_subsys->cpu.branch_mispredictions +=
-	      relay_subsys->cpu.branch_mispredictions;
+	      rhdl_subsys->cpu.branch_mispredictions;
 	    acct_subsys->cpu.wall_clock_time +=
-	      relay_subsys->cpu.wall_clock_time;
-	    acct_subsys->mem.alloc += relay_subsys->mem.alloc;
-	    acct_subsys->mem.freed += relay_subsys->mem.freed;
+	      rhdl_subsys->cpu.wall_clock_time;
+	    acct_subsys->mem.alloc += rhdl_subsys->mem.alloc;
+	    acct_subsys->mem.freed += rhdl_subsys->mem.freed;
 	  }
 
-	  // We have added the costs of relay_acct to acct, so can now reuse
-	  // relay_acct.
-          relay_acct->in_use = 0;
+	  // We have added the costs of rhdl_acct to acct, so can now reuse
+	  // rhdl_acct.
+          rhdl_acct->in_use = 0;
 
 	  // We have found a matching syscall_id, so we know it won't appear
 	  // again in the rest of the array, as syscall_id's are unique.
           return 0;
         } else {
-          relay_acct++;
+          rhdl_acct++;
           i++;
         }
       } else {
-        relay_acct++;
+        rhdl_acct++;
         i++;
       }
     }
   } else {
-    printf("relay_acct is null!");
+    printf("rhdl_acct is null!");
   }
   return -1;
 }

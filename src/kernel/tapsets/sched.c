c76f95977267fd37a7c831acd28950b3d3d5a1b9
Lucian Carata <lc525@cam.ac.uk>
2014-10-30 01:40:07 +0000

    fix on_cpu_switch to take into account that migration task can run on any CPU

--- src/kernel/tapsets/sched.c
+++ src/kernel/tapsets/sched.c
@@ -9,36 +9,56 @@
 void on_ctx_switch(pid_t next_tid) {
   pid_acct *it;
 
-  hash_for_each_possible(CPU_TBL(pid_acct_tbl), it, link, next_tid)
+  hash_for_each_possible(CPU_TBL(pid_acct_tbl), it, link, next_tid) {
     if(it->pid == next_tid){
       CPU_VAR(current_acct) = it;
       return;
     }
+  }
 
-  // next_tid is not in the hash table
+  // next_tid is not in the hash table (not a process using resourceful)
   CPU_VAR(current_acct) = NULL;
 }
 
 
 /* function that needs to be executed when a process/thread gets migrated from
- * cpu_from to cpu_to. Almost no overhead for processes that do not use
- * resourceful (just setting a kprobe)
+ * cpu_from to cpu_to. Two hash lookups overhead for processes that do not use
+ * resourceful.
+ *
+ * TODO(lc525, review discussion): we can turn this around and stop slowing
+ * down others (only one hash lookup) if we accept two hash lookups for pids
+ * using resourceful.
+ *
+ * this can execute on ANY cpu
  */
 void on_cpu_switch(int cpu_from, int cpu_to, pid_t pid) {
   pid_acct *it;
 
-  // Fastest path out: pid does not use resourceful
-  if(CPU_VAR(current_acct) == NULL) return;
-
   /* We assume that if the process is long-lived, after a while all CPUs will
    * have its pid_acct structure within their hash table. So the fast path is to
-   * just check for the existence of that entry and return. on_ctx_switch will
-   * take care of the rest (updating current_acct on the new cpu)
+   * just check for the existence of that entry in the destination CPU hash
+   * table and return. on_ctx_switch will take care of the rest (updating
+   * current_acct on the cpu)
    */
-  hash_for_each_possible(per_cpu(pid_acct_tbl, cpu_to), it, link, pid)
+  hash_for_each_possible(per_cpu(pid_acct_tbl, cpu_to), it, link, pid) {
     if(it->pid == pid){
       return;
     }
+  }
+
+
+  hash_for_each_possible(per_cpu(pid_acct_tbl, cpu_from), it, link, pid) {
+    if(it->pid == pid){
+      hash_add(per_cpu(pid_acct_tbl, cpu_to), &it->link, pid);
+      /* printk("CPU%d:[%d] migrate from CPU%d to CPU%d\n", cpu_to, pid,
+                cpu_from, cpu_to);*/
+      return;
+    }
+  }
+
+  /* Process does not use resourceful  */
+  return;
+}
 
   /* The hash table of the CPU we're moving on doesn't have the required entry.
    * Add it; at this point, we know current_acct != NULL

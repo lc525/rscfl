4c22a0e5c5977c668e32420db76d7ab2a59f735b
Oliver Chick <oc243@cam.ac.uk>
2014-11-01 15:16:21 +0000

    Fix types outputted to subsys_addr.h.
    Add appropriate casts, and fix types.

--- src/scripts/find_subsystems.py
+++ src/scripts/find_subsystems.py
@@ -38,15 +38,15 @@ rscfl_subsys_addr_template = """
 {% for subsystem in subsystems %}
 static kprobe_opcode_t {{ subsystem }}_ADDRS[] = {{ '{' }}
 {% for addr in subsystems[subsystem] %}
-  0x{{ addr }},
+  (kprobe_opcode_t)(0x{{ addr }} & 0xffffffff),
 {% endfor %}
   0
 {{ '};' }}
 {% endfor %}
 
-rscfl_addr_list *probe_addrs[NUM_SUBSYSTEMS] = {{ '{'  }}
+static kprobe_opcode_t *probe_addrs[] = {{ '{'  }}
 {% for subsys in subsystems %}
-  &{{ subsys }}_ADDRS,
+  {{ subsys }}_ADDRS,
 {% endfor %}
 {{ '};' }}
 #endif

100dfdf74f1f1108e5e09741d67c9209b9bece4f
Lucian Carata <lc525@cam.ac.uk>
2014-11-11 21:31:35 +0000

    add rscfl_init sanity test you might need to run `sudo make check` to run tests

--- src/tests/gtest_sanity_check.cpp
+++ src/tests/gtest_sanity_check.cpp
@@ -1,6 +1,14 @@
 #include "gtest/gtest.h"
 
+#include "rscfl/user/res_api.h"
+
 TEST(GtestSanityCheck, AlwaysPASS)
 {
     EXPECT_EQ(1, 1);
+}
+
+TEST(GtestSanityCheck, InitRscfl)
+{
+  ASSERT_TRUE(rscfl_init() != NULL) <<
+    "Have you loaded (insmod/staprun) the rscfl kernel module?";
 }

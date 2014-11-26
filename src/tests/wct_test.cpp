6654902bb3377897451222671eb48b0cf566f73c
James Snee <jas250@cam.ac.uk>
2014-11-26 01:15:22 +0000

    Fixed: formatting

--- src/tests/wct_test.cpp
+++ src/tests/wct_test.cpp
@@ -11,14 +11,14 @@
 
 class WCTTest : public testing::Test
 {
-  protected:
-    virtual void SetUp()
-    {
-      rhdl_ = rscfl_init();
-      ASSERT_NE(nullptr, rhdl_);
-    }
+ protected:
+  virtual void SetUp()
+  {
+    rhdl_ = rscfl_init();
+    ASSERT_NE(nullptr, rhdl_);
+  }
 
-    rscfl_handle rhdl_;
+  rscfl_handle rhdl_;
 };
 
 static struct timespec wct_test_get_time(void)
@@ -36,11 +36,11 @@ TEST_F(WCTTest, WallClock_Kernel_LT_User)
 {
   ASSERT_EQ(0, rscfl_acct_next(rhdl_));
 
-  struct timespec val_pre =  wct_test_get_time();
+  struct timespec val_pre = wct_test_get_time();
   int sockfd = socket(AF_LOCAL, SOCK_RAW, 0);
   EXPECT_GT(sockfd, 0);
-  struct timespec val_post =  wct_test_get_time();
-  ru64 user_time = (ru64) val_post.tv_nsec - val_pre.tv_nsec;
+  struct timespec val_post = wct_test_get_time();
+  ru64 user_time = (ru64)val_post.tv_nsec - val_pre.tv_nsec;
 
   struct accounting acct;
   ASSERT_EQ(0, rscfl_read_acct(rhdl_, &acct));
@@ -50,8 +50,9 @@ TEST_F(WCTTest, WallClock_Kernel_LT_User)
   struct subsys_accounting *subsys;
   rscfl_subsys curr_sub;
   for (int i = 1; i < NUM_SUBSYSTEMS; i++) {
-    if ((subsys = get_subsys_accounting(rhdl_, &acct, (rscfl_subsys)i)) != NULL) {
-      kernel_time += (ru64) subsys->cpu.wall_clock_time.tv_nsec;
+    if ((subsys = get_subsys_accounting(rhdl_, &acct, (rscfl_subsys)i)) !=
+        NULL) {
+      kernel_time += (ru64)subsys->cpu.wall_clock_time.tv_nsec;
     }
   }
 

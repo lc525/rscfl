7a67f6b77a8991cbd9a39e0c9090c4a9e845760e
Oliver Chick <oc243@cam.ac.uk>
2015-03-01 17:11:17 +0000

    Don't timestamp http requests.

--- src/scripts/web_replay/generate_requests.sh
+++ src/scripts/web_replay/generate_requests.sh
@@ -3,4 +3,4 @@
 # When given a squid proxy log as $1 this builds a file of the form
 # timestamp http://domain/filesize
 
- cat $1 | sed 's_^\([!0-9\.]*\) \([0-9]*\) .*\(http://[^/]*\).*$_\1 \3/\2 _'
+ cat $1 | sed 's_^[!0-9\.]* \([0-9]*\) .*\(http://[^/]*\).*$_\2/\1 _'

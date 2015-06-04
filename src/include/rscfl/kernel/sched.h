2d44f12cd386012e4f373691e3f6af740f9d3825
Oliver Chick <oc243@cam.ac.uk>
2015-06-04 18:18:58 +0100

    Tidy up includes in sched.h

--- src/include/rscfl/kernel/sched.h
+++ src/include/rscfl/kernel/sched.h
@@ -1,9 +1,7 @@
 #ifndef _RSCFL_SCHED_H_
 #define _RSCFL_SCHED_H_
 
-#include "rscfl/config.h"
-#include "rscfl/costs.h"
-#include "rscfl/kernel/hasht.h"
+#include "linux/sched.h"
 
 void on_ctx_switch(pid_t next_tid);
 void on_cpu_switch(int cpu_from, int cpu_to, pid_t pid);

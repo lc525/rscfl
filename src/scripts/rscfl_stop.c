ad66cd0d23b9d2a6e8c2a77ba393f251eed422cd
Lucian Carata <lc525@cam.ac.uk>
2015-07-29 02:52:38 +0000

    Adds script for calling the module shutdown ioctl

---
+++ src/scripts/rscfl_stop.c
@@ -0,0 +1,12 @@
+#include "rscfl/user/res_api.h"
+
+#include <sys/ioctl.h>
+
+int main(int argc, char* argv) {
+  int rc = 0;
+  rscfl_handle hdl = rscfl_get_handle();
+  if (hdl != NULL) {
+    rc = ioctl(hdl->fd_ctrl, RSCFL_SHUTDOWN_CMD);
+  }
+  return rc;
+}

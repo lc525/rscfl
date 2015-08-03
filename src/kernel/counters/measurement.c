0b7ecd7d9a39cc76174cc330bd2ab2ed9fe1bd20
Lucian Carata <lc525@cam.ac.uk>
2015-08-03 14:29:18 +0000

    Add build option to disable XEN-related code
    The XEN related code should be disabled when
    - running on top of unmodified XEN
    - running on top of unmodified kernel
    
    XEN requires to be built with rscfl-supporting changes
    The linux kernel needs to be built with a WARN_ON instead of a BUG_ON in
    ioremap_pte_range (http://lxr.free-electrons.com/source/lib/ioremap.c?v=3.19#L27)

--- src/kernel/counters/measurement.c
+++ src/kernel/counters/measurement.c
@@ -30,6 +30,7 @@
 #define CURRENT_XEN_NUM_EVENTS 256
 
 ru64 no_evtchn_events = 0;
+short disable_xen = NOT(XEN_ENABLED);
 
 struct sched_event
 {
@@ -101,7 +102,7 @@ static int xen_scheduler_init(void)
 
   struct page *pg;
 
-  if (*KPRIV(HYPERVISOR_shared_info) == KPRIV(xen_dummy_shared_info)) {
+  if (*KPRIV(HYPERVISOR_shared_info) == KPRIV(xen_dummy_shared_info) || disable_xen) {
     // Not running on Xen.
     return 0;
   }
@@ -189,7 +190,7 @@ int xen_buffer_hd(void)
       // by this amount.
       0x18);
 
-  if (*KPRIV(HYPERVISOR_shared_info) != KPRIV(xen_dummy_shared_info)) {
+  if (*KPRIV(HYPERVISOR_shared_info) != KPRIV(xen_dummy_shared_info) && !disable_xen) {
     // We are running in a Xen VM.
     return sched_info->sched_hd;
   } else {
@@ -241,7 +242,7 @@ int rscfl_counters_update_subsys_vals(struct subsys_accounting *add_subsys,
   }
 
   // HYPERVISOR
-  if (*KPRIV(HYPERVISOR_shared_info) != KPRIV(xen_dummy_shared_info)) {
+  if (*KPRIV(HYPERVISOR_shared_info) != KPRIV(xen_dummy_shared_info) && !disable_xen) {
     // We are running in a Xen VM.
 
     int hd = sched_info->sched_hd;

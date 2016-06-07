4a83fdf22e979b6a4cd8bc60da07c8ea2cbb2e10
Lucian Carata <lc525@cam.ac.uk>
2016-06-07 14:55:28 +0000

    remove printk message about number of subsys exits

--- src/kernel/acct.c
+++ src/kernel/acct.c
@@ -127,7 +127,7 @@ int clear_acct_next(void)
 
   current_pid_acct = CPU_VAR(current_acct);
   interest = &current_pid_acct->ctrl->interest;
-  printk("exits: %d\n", current_pid_acct->shared_buf->subsys_exits);
+  //printk("exits: %d\n", current_pid_acct->shared_buf->subsys_exits);
 #ifdef RSCFL_BENCH
   // clear the acct/subsys memory if the IST_CLEAR_FLAG was set
   //
